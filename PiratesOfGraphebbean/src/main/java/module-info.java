41119d643b163fe58c6302da4646b88f11704937
Arty4325 <arturo.acuna.duran@gmail.com>
2024-11-26 10:24:44 -0600

    Nodos conexos a fuente de energia

--- PiratesOfGraphebbean/src/main/java/module-info.java
+++ PiratesOfGraphebbean/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.example.piratesofgraphebbean {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires java.desktop;
 //    requires mail; TODO DESCOMENTAR ESTO
 
 
